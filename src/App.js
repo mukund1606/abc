07c2692c15eed610ee8287ada57a846273626871
Mukund Mittal <mukund.mukulmittal@gmail.com>
2023-07-29 00:39:34 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
-import"./app.css"
+import "./app.css";
+``;
 
 //import Article from "./Components/article/article"
-import Brand from "./Components/brand/Brand"
-import Cta from "./Components/cta/Cta"
+import Brand from "./Components/brand/Brand";
+import Cta from "./Components/cta/Cta";
 //import Feature from "./Components/feature/Feature"
-import Navbar from "./Components/navbar/Navbar"
-import Footer from "./containers/footer/Footer"
-import Header from "./containers/header/Header"
-import Features from "./containers/features/Features"
-import Whatgpt3 from "./containers/whatgpt3/Whatgpt3"
-import Possibilities from "./containers/possibilities/Possibilities"
-import Blog from "./containers/blog/Blog"
+import Navbar from "./Components/navbar/Navbar";
+import Footer from "./containers/footer/Footer";
+import Header from "./containers/header/Header";
+import Features from "./containers/features/Features";
+import Whatgpt3 from "./containers/whatgpt3/Whatgpt3";
+import Possibilities from "./containers/possibilities/Possibilities";
+import Blog from "./containers/blog/Blog";
+
 function App() {
   return (
     <div className="App">
       <div className="gradient__bg">
-      <Navbar/>
-        <Header/>
-
+        <Navbar />
+        <Header />
       </div>
-    <Brand/>
-    <Whatgpt3/>
-    <Features/>
-    <Possibilities/>
-    <Cta/>
-    <Blog/>
-    <Footer/>
+      <Brand />
+      <Whatgpt3 />
+      <Features />
+      <Possibilities />
+      <Cta />
+      <Blog />
+      <Footer />
     </div>
   );
 }
